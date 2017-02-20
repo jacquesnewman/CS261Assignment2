2f61652040df884feb64589838bfe27dccedd796
Johannes Peter <joshadow@gmail.com>
2017-02-20 04:20:46 -0800

    Using .env to load configs such as port, loglevel, etc.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// Load Environment Variables from .env file
+require('dotenv').config();
+let port     = process.env.NODE_PORT;
+let logLevel = process.env.NODE_LOG_LEVEL;
+
 let express = require('express');
 let path = require('path');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
+
 let auth = require('./utils/auth');
 let session = require('./models/session');
 let jsonUtils = require('./utils/json');
@@ -9,13 +15,15 @@ let users = require('./routes/users');
 
 let app = express();
 
-app.use(logger('dev'));
+app.use(logger(logLevel));
 app.use(bodyParser.json());
 app.use(jsonUtils.requestMiddleware);
 app.use(jsonUtils.responseMiddleware);
 
 users.register('/users/', app, auth.middleware);
 
-app.listen(7000);
+app.listen(port, function() {
+  console.log("Node app " + __filename + " is listening on port " + port + "!");
+});
 
 module.exports = app;
