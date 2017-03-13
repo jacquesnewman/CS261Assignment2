2b79ca33b1618d9338d8ed189d2eccb584e13a86
Stephen Beeman <stephen.beeman@gmail.com>
2017-03-13 05:48:00 -0700

    Creating gameserver

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ let auth = require('./utils/auth');
 let session = require('./models/session');
 let jsonUtils = require('./utils/json');
 let users = require('./routes/users');
+let gameserver = require('./routes/gameserver');
 
 let app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(logger(logLevel));
 app.use(bodyParser.json());
@@ -21,9 +25,11 @@ app.use(jsonUtils.requestMiddleware);
 app.use(jsonUtils.responseMiddleware);
 
 users.register('/users/', app, auth.middleware);
+gameserver.register('/game/', app, auth.middleware);
 
-app.listen(port, () => {
+let server = app.listen(port, () => {
   console.log("Node app " + __filename + " is listening on port " + port + "!");
 });
+gameserver.listen(server);
 
 module.exports = app;
---
+++ routes/gameserver.js
@@ -0,0 +1,31 @@
+let users = require('../models/user');
+let auth = require('../utils/auth');
+let common = require('../utils/common');
+let util = require('util');
+let moment = require('moment');
+let io = require('socket.io');
+
+let _root = '/';
+let _server = null;
+
+function doLanding(req, res, next) {
+    res.render('gameserver/landing', { });
+}
+
+module.exports.register = (root, app, authMiddleware) => {
+    _root = root;
+
+    app.all(_root, doLanding);
+}
+
+module.exports.listen = (server) => {
+    _server = io(server);
+
+    _server.on('connection', (socket) => {
+        console.log('CONNECTION ' + JSON.stringify(socket));
+
+        socket.on('disconnect', () => {
+            console.log('DISCONNECTION');
+        });
+    });
+}
