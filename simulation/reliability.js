60f954fd16b4c758231d49e394bcef0ac8829fc0
Stephen Beeman <stephen.beeman@gmail.com>
2017-03-27 20:51:16 -0700

    wip

--- simulation/reliability.js
+++ simulation/reliability.js
@@ -37,6 +37,7 @@ module.exports.begin = (replicationLayer) => {
                         let message = messages[i];
                         if (messages[i].substr(0,4) == 'MOVE')
                         {
+                            console.log(messages[i]);
                             let payload = inbound[i].substr(4).split('|', 1);
                             message = 'MOVE' + payload[1];
                             let acked = Number(payload[0]);
